be25eed5b3f74758fbc450c5f300d91ccc7e3145
rychtovy24 <rychtovy@gmail.com>
2019-03-22 10:14:25 +0100

    [TASK] add tables to create script
    add items, auction_rooms, auction_items, categories

--- src/main/sql/create_script.sql
+++ src/main/sql/create_script.sql
@@ -10,6 +10,41 @@ CREATE TABLE users
     age INTEGER
 );
 
+DROP TABLE if EXISTS categories CASCADE;
+CREATE TABLE categories
+(
+    id serial PRIMARY KEY,
+    name VARCHAR
+);
+
+DROP TABLE if EXISTS items CASCADE;
+CREATE TABLE items
+(
+    id serial PRIMARY KEY,
+    category_id INTEGER REFERENCES categories,
+    user_id INTEGER REFERENCES users,
+    count INTEGER,
+    name VARCHAR,
+    description VARCHAR
+);
+
+DROP TABLE if EXISTS auction_rooms CASCADE;
+CREATE TABLE auction_rooms
+(
+    id serial PRIMARY KEY,
+    category_id INTEGER REFERENCES categories
+);
+
+DROP TABLE if EXISTS auction_items CASCADE;
+CREATE TABLE auction_items
+(
+    id serial PRIMARY KEY,
+    room_id INTEGER REFERENCES auction_rooms,
+    item_id INTEGER REFERENCES items,
+    starting_price INTEGER,
+    min_bid_price INTEGER,
+    end_date DATE
+);
 -- DROP TABLE if EXISTS cities CASCADE;
 -- CREATE TABLE cities
 -- (
